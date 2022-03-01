e1b0f654900f91f340abea95e279eb09a6934d57
bruno.couto.acct <bruno.couto@acct.global>
2022-03-01 17:05:17 -0300

    feat: ending home and cart componente

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Sobre o desafio
+
+Nesse desafio, você deverá criar uma aplicação para treinar o que aprendeu até agora no ReactJS
+
+Essa será uma aplicação onde o seu principal objetivo é criar um hook de carrinho de compras. Você terá acesso a duas páginas, um componente e um hook para implementar as funcionalidades pedidas nesse desafio:
+
+- [x] Adicionar um novo produto ao carrinho;
+- [x] Remover um produto do carrinho;
+- [x] Alterar a quantidade de um produto no carrinho;
+- [x] Cálculo dos preços sub-total e total do carrinho;
+- [x] Validação de estoque;
+- [x] Exibição de mensagens de erro;
+- [ ] Entre outros.
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -19,15 +19,16 @@ interface CartContextData {
   updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
 }
 
-const CartContext = createContext<CartContextData>({} as CartContextData);
+const CartContext = createContext<CartContextData>({} as CartContextData)
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
-  const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+  const [cart, setCart] = useState<Product[]>(() => {
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
+
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
@@ -35,27 +36,78 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const addProduct = async (productId: number) => {
     try {
       // TODO
+      // * Get product data
+      const { data } = await api.get(`/products/${productId}`)
+      const productInCart = cart.find(product => product.id === productId)
+
+      if(productInCart) {
+        const { id, amount} = productInCart
+        const productAmountData = { productId: id, amount: amount + 1 }
+        updateProductAmount(productAmountData)
+      } else {
+        const productData: Product = {
+          ...data,
+          amount: 1
+        }
+        const productsCart = [...cart, productData]
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(productsCart))
+        setCart(productsCart)
+      }
     } catch {
       // TODO
+      toast.error('Erro na adição do produto')
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
+
+      const productInCart = cart.find(product => product.id === productId)
+
+      if(!productInCart){
+        toast.error('Erro na remoção do produto')
+        throw new Error("Erro na remoção do produto")
+      }
+
       // TODO
+      const newCart = cart.filter(product => product.id !== productId)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart))
+      setCart(newCart)
     } catch {
       // TODO
+       toast.error('Erro na remoção do produto')
     }
   };
 
-  const updateProductAmount = async ({
-    productId,
-    amount,
-  }: UpdateProductAmount) => {
+  const updateProductAmount = async ({productId, amount}: UpdateProductAmount ) => {
     try {
       // TODO
+
+      if (amount < 1) {
+        throw new Error('Alteraçáo inválida')
+      }
+
+      const { data } = await api.get(`/stock/${productId}`)
+      const limit = data.amount
+
+      if(amount > limit) {
+        toast.error('Quantidade solicitada fora de estoque')
+      } else {
+        const newCart:Product[] = cart.map(product => {
+          if (product.id === productId){
+            return {
+              ...product,
+              amount
+            }
+          }
+          return product
+        })
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart))
+        setCart(newCart)
+      }
     } catch {
       // TODO
+      toast.error('Erro na alteração de quantidade do produto')
     }
   };
 
